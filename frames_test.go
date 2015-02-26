c1965cadacdcd29bd035174b78528209aabf636d
Tristan Colgate <tristan@we7.com>
2015-02-26 20:56:21 +0000

    Add an example for the docs

--- frames_test.go
+++ frames_test.go
@@ -2,16 +2,37 @@ package mp3
 
 import (
 	"bytes"
-	"log"
+	"fmt"
+	"os"
 	"testing"
 )
 
 func BenchmarkDecode(t *testing.B) {
+	// I suspect the compiler is actually just eliding
+	// this
 	r := bytes.NewReader(SilentBytes)
 	d := NewDecoder(r)
 
 	f := Frame{}
 	d.Decode(&f)
+}
 
-	log.Println(&f)
+func ExampleDecode() {
+	r, err := os.Open("file.mp3")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	d := NewDecoder(r)
+	var f Frame
+	for {
+
+		if err := d.Decode(&f); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(&f)
+	}
 }
